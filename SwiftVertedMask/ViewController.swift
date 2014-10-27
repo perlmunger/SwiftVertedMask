e2f5d45b200fb5a30c6fd938989a873844b7fa46
Matt Long <matt.long@matthew-long.com>
2014-10-27 12:58:16 -0600

    Initial Commit

---
+++ SwiftVertedMask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftVertedMask
+//
+//  Created by Matt Long on 10/27/14.
+//  Copyright (c) 2014 Skye Road Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
